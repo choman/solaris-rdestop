3547661b75bbedf8f5a49506c11fb497bd71cacf
Chad Homan <choman@gmail.com>
2017-02-09 13:10:32 -0600

    Updated for build

--- build.sh
+++ build.sh
@@ -1,2 +1,7 @@
-LD_LIBRARY_PATH=/usr/lib 
-./configure --enable-smartcard --with-openssl=/usr/sfw --disable-credssp
+#!/usr/bin/bash
+
+export PCSCLITE_LIBS="-L/usr/lib -lpcsclite -lpthread" 
+export PCSCLITE_CFLAGS="-I/usr/include/smartcard"
+
+./configure --enable-smartcard --with-openssl=/usr/sfw --disable-credssp 
+
